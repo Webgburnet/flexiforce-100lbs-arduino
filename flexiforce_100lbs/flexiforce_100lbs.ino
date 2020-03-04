fdaa814647522fa921752d59e411bb7d480eac53
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2020-03-04 08:32:13 +0100

    Add files via upload

---
+++ flexiforce_100lbs/flexiforce_100lbs.ino
@@ -0,0 +1,35 @@
+#define FSR_PIN A0
+
+const float VCC = 5040; // tension alim arduino en mV
+const float R_DIV = 96400.0; // Resisantce 100k
+const float g = 9.81 ;
+const float coef = 0.1;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(FSR_PIN, INPUT);
+}
+
+void loop() 
+{
+  int fsrADC = analogRead(FSR_PIN);
+  float fsr_vout = map(fsrADC, 0,1023,0, VCC);
+  float fsr_R = R_DIV * VCC/fsr_vout;
+  float kg = fsrADC*coef;
+  float force = g*kg;
+  Serial.print(" Sortie Analogique : ");
+  Serial.print(fsrADC);
+  Serial.print(" \t Tension sortie : ");
+  Serial.print(fsr_vout);
+  Serial.print(" mV \t R : ");
+  Serial.print(fsr_R);
+  Serial.print(" Ohm \t Masse : ");
+  Serial.print(kg);
+  Serial.print(" kg \t Force : ");
+  Serial.print(force);
+  Serial.println(" N");
+  
+  
+  
+}
